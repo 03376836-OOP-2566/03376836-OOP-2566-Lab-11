2acb895d3fb77bb807b584379b12e938d3752bd8
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-03-22 04:29:51 +0700

    03376836-OOP-2566-Lab-11

---
+++ 0289/Lab11_Ex02/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.WriteLine(Calculator.AddValues(2, 3));
+System.Console.WriteLine(Calculator.AddValues(2.0f, 3.0f));
+System.Console.WriteLine(Calculator.AddValues(2L, 3L));
+
+public static class Calculator
+{
+    public static int AddValues(int a, int b)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(int a, int b)");
+        return a + b;
+    }
+    public static long AddValues(int c, int d)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(int c, int d)");
+        return c + d;
+    }
+    public static float AddValues(int f, int g)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(int f, int g)");
+        return (long) (f + g);
+    }
+   public static long AddValues(long h, long i)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(long h, long i)");
+        return (long) (h + i);
+    }
+}
